a54ff4ecaac77e3fd1a4da981e0177a8c92c7a92
Michał Wargan <mwargan@pc61.home>
2018-12-12 23:31:57 +0100

    Removed SW while fixing redirect loop on logout

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -50,19 +50,9 @@
   gtag('js', new Date());
 
   gtag('config', 'UA-76122959-2');
-
-  // Facebook Pixel Code
-
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '224430568407639');
-  fbq('track', 'PageView');
+</script>
+  <!-- Facebook Pixel Code -->
+<script>
 
  // Hotjar Tracking Code for justbookr.com
     (function(h,o,t,j,a,r){
