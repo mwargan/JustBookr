d428ab88c8fe678315d50d9f271cbfe9e41c787f
Michał Wargan <mwargan@pc61.home>
2018-12-13 20:15:34 +0100

    Fixed SW reload loop and mailchimp tags

--- public/sw.js
+++ public/sw.js
@@ -29,8 +29,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/https://justbookr.com/");
-
 workbox.routing.registerRoute(/https:\/\/justbookr.com/, workbox.strategies.networkFirst({ cacheName: "JustBookr-production", plugins: [] }), 'GET');
 workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|svg)$/, workbox.strategies.cacheFirst(), 'GET');
 workbox.routing.registerRoute(/https:\/\/fonts.(googleapis|gstatic).com/, workbox.strategies.cacheFirst({ cacheName: "google-fonts", plugins: [] }), 'GET');
+workbox.routing.registerNavigationRoute("/https://justbookr.com/");
